f753f29a6fa5b4942e776f6be3d4ec68e5939420
StructuredTrading <samowen1992@hotmail.com>
2024-05-09 20:32:28 +1000

    feat: edit in place completed

--- src/components/SocialCardParent.jsx
+++ src/components/SocialCardParent.jsx
@@ -1,26 +1,39 @@
-import React from "react"
-import SocialCardDisplay from "./SocialCardDisplay";
+import React from "react";
 import SocialCardForm from "./SocialCardForm";
-
+import SocialCardDisplay from "./SocialCardDisplay";
 
 export default class SocialCardParent extends React.Component {
-    constructor() {
-        super();
+	constructor(){
+		super();
 
-        this.state= {
-            currentlyEditing: false,
-            content: "Default content",
-            imageUrl: ""
-        }
-    }
+		this.state = {
+			currentlyEditing: true,
+			content: "Default content",
+			imageUrl: ""
+			
+		}
+	}
 
-    render() {
-        if (this.state.currentlyEditing){
-            return <SocialCardForm contents={this.state.content} imageUrl={this.state.imageUrl} toggleEditMode="" />
-        } else {
-            return <SocialCardDisplay contents={this.state.content} imageUrl={this.state.imageUrl} toggleEditMode="" />
-    
-        }
-    }
+	updateStateProperty = (targetStateKey, newStateValue) => {
+		console.log(targetStateKey, newStateValue);
+		this.setState({
+			[targetStateKey]: newStateValue, // <-- Finds state key as a variable and updates that
+			// targetStateKey: newStateValue <-- This creates a key in state called "targetStateKey"
+		})
+	}
 
+	toggleEditMode = () => {
+		this.setState({currentlyEditing: !this.state.currentlyEditing});
+	}
+
+
+
+	render(){
+		if (this.state.currentlyEditing){
+			return <SocialCardForm toggleEditMode={this.toggleEditMode} setParentState={this.updateStateProperty} content={this.state.content} imageUrl={this.state.imageUrl}  />
+		} else {
+			return <SocialCardDisplay toggleEditMode={this.toggleEditMode} content={this.state.content} imageUrl={this.state.imageUrl}  />
+		}
+	}
 }
+
